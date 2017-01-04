17e4377790441060de65773fc392ad0fba466460
Gw0496 <gw0496@gmail.com>
2017-01-04 13:41:57 +0900

    平均化しました

--- R/GBPJPY_MARGE_V6.R
+++ R/GBPJPY_MARGE_V6.R
@@ -7,10 +7,6 @@ dt_all <- data.frame()
 # 読み込んだcsvをマージする
 dt_ashi15 <- read.table("GBPJPY15.csv", header=F, sep=",")
 
-# 平均化
-ashi15Aves <- colMeans(dt_ashi15[,3,drop=FALSE])
-dt_ashi15[,c(3,4,5,6)] <-  dt_ashi15[,c(3,4,5,6)] - ashi15Aves
-
 nrow_ashi15   <- nrow(dt_ashi15)-31 #最後の31行（項目が30個なので）まで
 i15 <- 0
 # # 空行テーブル
@@ -22,10 +18,17 @@ for(i15 in 1:nrow_ashi15){
    str_date <- as.character(dt_ashi15[i15,1]) #日
    str_time <- as.character(dt_ashi15[i15,2]) #時
    dt_endprice <- t(dt_ashi15[c(i15:i15e),6]) #終値
-   
-   dt_record <- data.frame(cbind(str_date,str_time,dt_endprice))
+
+   # 平均化
+   dt_endprice2 <- dt_endprice - dt_endprice[,c(30)]
+   dt_endprice3 <- round(dt_endprice2,digits=3) # 浮動小数点のせいか？やらないと小数点以下がいっぱい。
+
+   dt_record <- data.frame(cbind(str_date,str_time,dt_endprice3))
+
    dt_all <- rbind(dt_all,dt_record)
 }
+
+
 # write.dt(dt_all, "output.txt", quote=F,col.names=F, append=F,row.names=F)
-write.csv(dt_all, "GBPJPYALL.csv", quote=F,row.names=F)
+write.csv(dt_all, "GBPJPYALL2.csv", quote=F,row.names=F)
 
