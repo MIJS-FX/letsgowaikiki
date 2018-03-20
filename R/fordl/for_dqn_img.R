5f0ea6a15e01a4b032594013b050a7c7fbba5e91
Takeshi_Umezaki <Takeshi_Umezaki@sis2.saison.co.jp>
2018-03-20 18:59:50 +0900

    溜まっていたので

--- R/fordl/for_dqn_img.R
+++ R/fordl/for_dqn_img.R
@@ -17,3 +17,8 @@ cn=c("Date","Time","Open","High","Low","Close","Volume") #MT4のcsvのラベル
 data <- read.zoo("GBPJPY2.csv",sep=",",header=T,index=1:2,tz="",format="%Y.%m.%d%H:%M") #
 
 candleChart(data,subset='2017-02-01 19:10:00::2017-02-01') #指定の範囲でローソク足チャート表示
+dev.off()        
+
+plot(sin, -pi, 2*pi)                                   # plot(関数名, 下限, 上限)
+gauss.density <- function(x) 1/sqrt(2*pi)*exp(-x^2/2)  # 標準正規分布の密度
+plot(gauss.density,-3,3)
--- R/fordl/for_dqn_img_gbp5m.R
+++ R/fordl/for_dqn_img_gbp5m.R
@@ -1,27 +1,13 @@
-#import quantmod
-
 library(quantmod)
 library(data.table)
 library(dplyr)
-library("magick")
+library(magick)
 
 cn=c("Date","Time","Open","High","Low","Close","Volume") #MT4のcsvのラベル
 
 data <- read.zoo("GBPJPY_15_201703.csv",sep=",",header=F,index=1:2,tz="",format="%Y.%m.%d%H:%M",col.names=cn) #
-
-
-# #subseted_data <- data[1:20]
-# par(xaxt="n")
-# par(yaxt="n")
-# #plt = c(0, 1, 0, 1)
-# #par("usr")
-# #mai = c(0, 0, 0, 0)
-# candleChart(subseted_data)
-# #chartSeries(subseted_data)
-
-# jpeg保存にする
-
-for(i in 1:100){
+i <- 0
+for(i in 1:20){
   first_line <- i
   last_line <- i + 20
   subseted_data <- data[first_line:last_line]
@@ -29,13 +15,11 @@ for(i in 1:100){
   candleChart(subseted_data)
   dev.off()
   # 日時ファイル名。断念
-  #f_name = candle + print(subseted_data[1,1])
   f_name <- paste("candle",sprintf("%05d",i),".jpg",sep = "")
   
-  #dev.copy(jpeg,"candle.jpg",width=200,height=200)
-  #dev.off()
   cnd_img <- image_read("candle.jpg")
   cnd_img_s <- image_crop(image = cnd_img, geometry = "103x123+51+29")
   image_write(cnd_img_s, path = f_name, format = "jpg")
 }
 dev.off()
+
